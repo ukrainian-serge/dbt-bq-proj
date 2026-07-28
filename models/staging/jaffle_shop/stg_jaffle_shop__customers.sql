81de1f3a53144f35e215b0a8863a354108920745
serge <39893631+ukrainian-serge@users.noreply.github.com>
2026-07-28 18:08:17 -0400

    built staging tables for customers and orders. still more work to do in the testing yml. need to set dtyps as well.

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,5 +1,25 @@
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from {{ source('jaffle_shop', 'customers') }}
+WITH stg AS (
+
+select
+    id as customer_id,
+    name as first_and_last_name,
+from {{ source('jaffle_shop', 'customers') }}
+
+)
+
+, trimmed as (
+    SELECT 
+        customer_id,
+        TRIM(first_and_last_name) as first_and_last_name
+    FROM stg
+)
+
+, final as (
+    SELECT 
+        customer_id,
+        split(first_and_last_name, ' ')[safe_offset(0)] as first_name,
+        split(first_and_last_name, ' ')[safe_offset(1)] as last_name,
+    FROM trimmed
+)
+
+SELECT * FROM final
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,25 @@
+WITH stg AS (
+    select
+        id,
+        customer,
+        CAST(ordered_at AS DATE) as ordered_at,
+        store_id,
+        CAST(subtotal AS integer) as subtotal,
+        CAST(tax_paid AS integer) as tax_paid,
+        CAST(order_total AS integer) as order_total
+    from {{ source('jaffle_shop', 'orders') }}
+)
+
+, final AS (
     select
         id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from {{ source('jaffle_shop', 'orders') }}
+        customer as customer_id,
+        ordered_at,
+        store_id,
+        SAFE_DIVIDE(subtotal, 100) as subtotal,
+        SAFE_DIVIDE(tax_paid, 100) as tax_paid,
+        SAFE_DIVIDE(order_total, 100) as order_total
+    from stg
+)
+
+SELECT * FROM final
