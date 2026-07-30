a7a61a88bf0307054795ab6dbab7c377e958872d
serge <39893631+ukrainian-serge@users.noreply.github.com>
2026-07-30 14:26:39 -0400

    refactored fct_customer_orders. no tests for it, but tested preview and build, both have no errors.

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,38 +1,58 @@
-with orders_enriched as (
-    select
+with 
+
+customers AS (
+    SELECT * FROM {{ ref('dim_customers') }}
+)
+
+, orders AS (
+    SELECT * FROM {{ ref('fct_orders') }}
+)
+
+
+
+, customer_orders AS (
+    SELECT
         o.order_id,
         o.customer_id,
-        o.ordered_at as order_placed_at,
-        o.order_total as total_amount_paid,
         c.first_name as customer_first_name,
-        c.last_name as customer_last_name,
+        c.last_name as customer_last_name,   
+        o.order_total as order_total,
 
-        -- Aggregations replaced with lightweight window functions
         min(o.ordered_at) over (partition by o.customer_id) as cust_first_order_date,
         max(o.ordered_at) over (partition by o.customer_id) as cust_most_recent_order_date,
         count(o.order_id) over (partition by o.customer_id) as cust_lifetime_order_count,
+        o.ordered_at as order_date,
+
+    from orders as o
+    left join customers as c on o.customer_id = c.customer_id
+    GROUP BY ALL
+
+)
+
+
+
+, final as (
+    select 
+        *,
+        case
+            when order_date = cust_first_order_date then 'new'
+            else 'return'
+        end as nvsr,
 
         -- Order sequence & running CLV
         row_number() over (
-            partition by o.customer_id 
-            order by o.ordered_at, o.order_id
+            partition by customer_id
+            order by order_date,
+                order_id
         ) as customer_sales_seq,
-        
-        sum(o.order_total) over (
-            partition by o.customer_id 
-            order by o.ordered_at, o.order_id
-            rows between unbounded preceding and current row
+
+        sum(order_total) over (
+            partition by customer_id
+            order by order_date,
+                order_id rows between unbounded preceding and current row
         ) as customer_lifetime_value
 
-    from {{ ref('fct_orders') }} as o
-    left join {{ ref('dim_customers') }} as c
-        on o.customer_id = c.customer_id
+    from customer_orders
 )
 
-select
-    *,
-    case 
-        when cust_first_order_date = order_placed_at then 'new'
-        else 'return' 
-    end as nvsr
-from orders_enriched
+select * from final
