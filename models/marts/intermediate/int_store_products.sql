ab7b241d9f40feb653b5a5acc275fb58dfbfb54f
serge <39893631+ukrainian-serge@users.noreply.github.com>
2026-08-26 14:33:08 -0400

    slight refactor

--- models/marts/intermediate/int_store_products.sql
+++ models/marts/intermediate/int_store_products.sql
@@ -1,4 +1,11 @@
-{% set get_products_query %}
+-- jinja here grabs distinct product_name from stg_jaffle_shop__products
+{% set product_names = dbt_utils.get_column_values(
+    table=ref('stg_jaffle_shop__products'),
+    column='product_name',
+    default=['product_test_dummy1', 'product_test_dummy2', 'product_test_dummy3']
+) %}
+
+{# {% set get_products_query %}
     select distinct product_name 
     from {{ ref("stg_jaffle_shop__products") }} 
     where product_name is not null 
@@ -8,15 +15,13 @@
 {% if execute %}
     {% set results = run_query(get_products_query) %}
     {% set product_names = results.columns[0].values() %}
-{% endif %}
-
+{% endif %} #}
 
 with
     cte_orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
     cte_stores as (select * from {{ ref("stg_jaffle_shop__stores") }}),
     cte_items as (select * from {{ ref("stg_jaffle_shop__items") }}),
     cte_products as (select * from {{ ref("stg_jaffle_shop__products") }}),
-
     cte_joined as (
         select
             b.store_name,
@@ -24,11 +29,9 @@ with
             c.product_type,
             c.description as product_description,
             a.order_total
-
         from cte_orders as a
         left join cte_stores as b on a.store_id = b.store_id
-        left join
-            (
+        left join (
                 select distinct
                     a.order_id, b.product_name, b.product_type, b.description
                 from cte_items as a
@@ -39,34 +42,24 @@ with
     cte_pivot as (
         select
             coalesce(store_name, 'TOTAL') as store_name,
+            {% for product_name in product_names -%}
+            round(
+                sum(
+                    case when product_name = '{{ product_name }}'
+                         then order_total
+                         else 0
+                         end
+                ), 2 ) as {{ product_name | replace("-", "_") | replace(" ", "_") | lower }},
+            {% endfor -%}
 
-            {% for product_name in product_names %}
-                round(
-                    sum(
-                        case
-                            when product_name = '{{ product_name }}'
-                            then order_total
-                            else 0
-                        end
-                    ),
-                    2
-                ) as {{ product_name | replace("-", "_") | replace(" ", "_") | lower }}
-                ,
-            {% endfor %}
-
-            round(sum(order_total), 2) as total_store_sales
+            round(sum(order_total), 0) as TOTAL
 
         from cte_joined
         group by rollup (store_name)
     ),
 
     cte_final as (
-        select 
-            coalesce(store_name, 'TOTAL') as store_name, 
-            * except (store_name)
-
-
-            
+        select coalesce(store_name, 'TOTAL') as store_name, * except (store_name)
         from cte_pivot
     )
 
--- models/test_code.sql
+++ models/test_code.sql
@@ -1,4 +1,4 @@
-{% set get_products_query %}
+{# {% set get_products_query %}
     select distinct product_name 
     from {{ ref("stg_jaffle_shop__products") }} 
     where product_name is not null 
@@ -8,13 +8,14 @@
 {% if execute %}
     {% set results = run_query(get_products_query) %}
     {% set product_names = results.columns[0].values() %}
-{% endif %}
+{% endif %} #}
 
 {# If the query returns empty or runs during dbt compile, use default fallbacks #}
 {# {% if not product_names or product_names | length == 0 %}
     {% set product_names = ["item_a", "item_b"] %}
 {% endif %} #}
-select
+
+{# select
     store_id,
     {% for product_name in product_names %}
         sum(case when product_name = '{{ product_name }}' then order_total else 0 end)
@@ -23,4 +24,24 @@ select
     {% endfor %}
 
 from {{ ref("stg_jaffle_shop__orders") }}
-group by 1
+group by 1 #}
+
+{# SELECT 
+  grantee AS principal,
+  role_name AS role,
+  table_catalog AS project_id,
+  table_schema AS dataset_name
+FROM `dbt-dev-503215.INFORMATION_SCHEMA.SCHEMATA_SHARED_TO_SUBSUMED`
+WHERE table_schema = 'int_store_products'; #}
+
+
+{# with cte as ( #}
+    select 
+        *
+    from `{{ target.database }}`.`{{ target.schema }}`.INFORMATION_SCHEMA.TABLES
+{# )
+
+select
+    object_type,
+    concat('DROP ', object_type, ' `', project_id, '`.`', dataset_id, '`.`', table_id, '`;') as drop_statement
+from cte #}
